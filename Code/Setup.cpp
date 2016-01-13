d0b7ce27961ab672d4697f0ff6c25612e0f39e71
Graham <graham.a.ross@gmail.com>
2016-01-12 22:11:19 -0500

    Added license

--- Code/Setup.cpp
+++ Code/Setup.cpp
@@ -11,30 +11,16 @@
 	See the License for the specific language governing permissions and
 	limitations under the License.
 */
-#pragma once
 
-#include <ESP8266WiFi.h>
-#include "RtuMaster.h"
+#include "Setup.h"
 
-#define MODBUSIP_PORT 	  502
-#define MODBUSIP_MAXFRAME 200
-#define MODBUSIP_TIMEOUT   10
-#define TCP_BUFFER_SIZE 260
 
-class TcpSlave
+
+Setup::Setup()
 {
-private:
-	byte _MBAP[7];
-	byte _sendbuffer[TCP_BUFFER_SIZE];
-	WiFiClient _client;
-	byte *_frame;
-	byte  _len;
-	RtuMaster _rtuMaster;
+}
 
-public:
-	TcpSlave();
-	~TcpSlave();
-	void config(const char* ssid, const char* password, long baudRate);
-	void task();
-};
 
+Setup::~Setup()
+{
+}
--- Code/Setup.h
+++ Code/Setup.h
@@ -12,29 +12,10 @@
 	limitations under the License.
 */
 #pragma once
-
-#include <ESP8266WiFi.h>
-#include "RtuMaster.h"
-
-#define MODBUSIP_PORT 	  502
-#define MODBUSIP_MAXFRAME 200
-#define MODBUSIP_TIMEOUT   10
-#define TCP_BUFFER_SIZE 260
-
-class TcpSlave
+class Setup
 {
-private:
-	byte _MBAP[7];
-	byte _sendbuffer[TCP_BUFFER_SIZE];
-	WiFiClient _client;
-	byte *_frame;
-	byte  _len;
-	RtuMaster _rtuMaster;
-
 public:
-	TcpSlave();
-	~TcpSlave();
-	void config(const char* ssid, const char* password, long baudRate);
-	void task();
+	Setup();
+	~Setup();
 };
 
